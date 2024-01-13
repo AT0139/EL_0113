f5ac6e8e45c384cb984c41e42703bebcd9b35b11
堤田芙美子 <fengritian93@gmail.com>
2024-01-13 14:50:08 +0900

    ＜追加＞結果に合わせて画像変更

--- Assets/Script/Result/ResultManager.cs
+++ Assets/Script/Result/ResultManager.cs
@@ -10,9 +10,25 @@ using UnityEngine.UI;
 public class ResultManager : MonoBehaviour
 {
     [SerializeField] private Text _text;
+    [SerializeField] private Text _resultText;
+    [SerializeField] private Image _resultImg;
+    [SerializeField] private Sprite[] _resultchar = new Sprite[2];
 
     void Start()
     {
+       var _dontDestroyData = GameObject.Find("DontDestroyData").GetComponent<DontDestroyData>();
+
+        if (_dontDestroyData.isCatWon)
+        {
+            _resultText.text = "Cat WIN";
+            _resultImg.sprite = _resultchar[0];
+        }
+        else
+        {
+            _resultText.text = "SHRIMP WIN";
+            _resultImg.sprite = _resultchar[1];
+        }
+
         _text.gameObject.SetActive(false);
 
         Observable.Timer(TimeSpan.FromSeconds(3)).Subscribe(_ =>
